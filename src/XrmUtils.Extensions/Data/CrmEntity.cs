fd78390103a010ee17fb9186c14229632e15e9b3
Emerson Brito <emerbrito@users.noreply.github.com>
2018-04-10 16:56:00 -0400

    CrmEntity methods to better handle primary key and emum proeprties.

--- src/XrmUtils.Extensions/Data/CrmEntity.cs
+++ src/XrmUtils.Extensions/Data/CrmEntity.cs
@@ -107,6 +107,50 @@ namespace XrmUtils.Extensions.Data
             return this.GetAttributeValue<T>(propertyName, image, defaultValue);
         }
 
+        protected TEnum? GetEnumAttributeValue<TEnum>(string attributeLogicalName, TEnum? defaultValue = null) where TEnum : struct, IConvertible
+        {
+
+            var att = GetAttributeValue<OptionSetValue>(attributeLogicalName);
+
+            if (att == null)
+            {
+                if (defaultValue.HasValue)
+                {
+                    return defaultValue;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            return (TEnum)(object)att.Value;
+        }
+
+        protected void SetEnumPropertyValue<TEnum>(string propertyName, string attributeName, TEnum? value, TEnum? defaultValue = null)
+            where TEnum : struct, IConvertible
+        {
+
+            var opvalue = default(OptionSetValue);
+
+            if (value.HasValue)
+            {
+                opvalue = new OptionSetValue((int)(object)value);
+            }
+            else if (defaultValue.HasValue)
+            {
+                opvalue = new OptionSetValue((int)(object)defaultValue);
+            }
+
+            SetPropertyValue(nameof(propertyName), attributeName, opvalue);
+        }
+
+        protected void SetIdPropertyValue(string propertyName, string attributeName, Guid? value)
+        {
+            SetPropertyValue(propertyName, attributeName, value);
+            base.Id = value ?? Guid.Empty;
+        }
+
         protected void SetPropertyValue(string propertyName, string attributeName, object value)
         {
             this.OnPropertyChanging(propertyName);
